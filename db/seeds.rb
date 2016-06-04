44f84b344e29cfc70ad4d4ac5587ba2595df8df2
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-03 23:33:11 -0400

    Add script to seed a user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user_1 = User.create(email: 'tester@test.co', password: 'test')
