000d74cee3827670d4b4dce182423175f28c1ecf
olivia-leach <olivia.i.m.leach@gmail.com>
2016-06-05 17:08:23 -0400

    Add curl scripts for sign in and get buildings

---
+++ scripts/get_buildings.sh
@@ -0,0 +1,2 @@
+curl --include --request GET http://localhost:3000/buildings \
+  --header "Authorization: Token token=BAhJIiVlZmYwY2ZkNzRkOTkxNzhhMGM4NjhhZjAzZWVjYmQ5OQY6BkVG--bba07e3201bd95dc2e981eca0524b4e3b4d216a8"
